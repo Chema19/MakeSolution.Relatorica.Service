fd746764ee1656d82c64d02fe3b5bd6f4bf191d2
josemaria inga villafuerte <chemalonso96@gmail.com>
2019-09-18 17:08:56 -0500

    token validation

---
+++ MakeSolution.Relatorica.Entities/SonidoEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeSolution.Relatorica.Entities
+{
+    public class SonidoEntity
+    {
+        public Int32? SonidoId { set; get; }
+        public String Nombre { set; get; }
+        public String Url { set; get; }
+        public Int32? GeneroId { set; get; }
+        public String Estado { set; get; }
+        public DateTime FechaRegistro { set; get; }
+        public Int32? UsuarioId { set; get; }
+    }
+}
